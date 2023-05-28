83c39d8a00ff3c18fe76f682e7a1831aaa9fca2d
JESSE <jessejzee@gmail.com>
2023-05-29 00:44:37 +0300

    connected to the database

--- appfront/src/components/Card/Card.jsx
+++ appfront/src/components/Card/Card.jsx
@@ -3,20 +3,41 @@ import "./Card.scss";
 import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
+
+  const number = item?.attributes?.images?.data?.length || 0;
+  const randomIndex = Math.floor(Math.random() * number);
+
+  const img2 = randomIndex ===0 ? randomIndex + 1 : randomIndex - 1
   
   return (
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
-        {item.isnew  && <span>New arrivals</span>}
-            <img src={item.img} className="mainimg" alt="" />
-            <img src={item.img2} className="secondImg" alt="" />
+          {item.attributes.isNew && <span>New arrivals</span>}
+
+          <img
+            src={
+              process.env.REACT_APP_SERVER +
+              item?.attributes?.images?.data[
+                randomIndex
+              ].attributes?.url
+            }
+            className="mainImg"
+            alt=""
+          />
+          <img
+            src={
+              process.env.REACT_APP_SERVER +
+              item?.attributes?.images?.data[img2].attributes.url
+            }
+            className="secondImg"
+            alt=""
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item.attributes.title}</h2>
         <div className="prices">
-<h3>{item.oldPrice}</h3>
-<h3>{
-item.price}</h3>
+          <h3>{item.attributes.oldPrice || item.attributes.price + 10}</h3>
+          <h3>{item.attributes.price}</h3>
         </div>
       </div>
     </Link>
